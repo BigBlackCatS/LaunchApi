6fb027ef4e4c83fc30c6e52167341b9d61a52d91
Svetlana Matviienko <svitlana.matviienko@hys-enterprise.com>
2023-07-23 15:24:24 +0300

    Added a Launch Service for processing the API Client responses.
    Added a Launch Service for processing the API Client responses.

---
+++ LaunchApi/Services/ILaunchesService.cs
@@ -0,0 +1,13 @@
+﻿using LaunchApi.Domain.Models;
+
+namespace LaunchApi.Services
+{
+    public interface ILaunchesService
+    {
+        Task<PagedResult<Launch>> GetPastLaunchesAsync(uint offset, uint limit, CancellationToken cancellationToken = default);
+
+        Task<PagedResult<Launch>> GetUpcomingLaunchesAsync(uint offset, uint limit, CancellationToken cancellationToken = default);
+
+        Task<Launch> GetLaunchAsync(string id, CancellationToken cancellationToken = default);
+    }
+}
---
+++ LaunchApi/Services/LaunchesService.cs
@@ -0,0 +1,34 @@
+﻿using LaunchApi.ApiClients.v1;
+using LaunchApi.Domain.Models;
+
+namespace LaunchApi.Services
+{
+    public class LaunchesService : ILaunchesService
+    {
+        public readonly ILaunchesApiClient _launchApiClient;
+        public LaunchesService(ILaunchesApiClient launchApiClient)
+        {
+            _launchApiClient= launchApiClient;
+        }
+
+        public async Task<PagedResult<Launch>> GetPastLaunchesAsync(uint offset, uint limit, CancellationToken cancellationToken = default)
+        {
+            return await _launchApiClient.GetPastLaunchesAsync(offset, limit, cancellationToken);
+        }
+
+        public async Task<PagedResult<Launch>> GetUpcomingLaunchesAsync(uint offset, uint limit, CancellationToken cancellationToken = default)
+        {
+            return await _launchApiClient.GetUpcomingLaunchesAsync(offset, limit, cancellationToken);
+        }
+
+        public async Task<Launch> GetLaunchAsync(string id, CancellationToken cancellationToken = default)
+        {
+            if (id.Equals(string.Empty))
+            {
+                throw new ArgumentNullException(nameof(id), "The flight number can't be empty.");
+            }
+
+            return await _launchApiClient.GetLaunchAsync(id, cancellationToken);
+        }
+    }
+}
